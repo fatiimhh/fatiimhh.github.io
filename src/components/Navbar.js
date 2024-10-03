eda15fcbbb295c7495c916f72b8f631042ace83b
Fatima Bani Sakher <fatima.mbk00@gmail.com>
2024-10-03 20:54:39 +0300

    Navbar updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,19 +2,12 @@ import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-//import logo from "../Assets/logo.png";
-import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
-import { CgGitFork } from "react-icons/cg";
-//import { ImBlog } from "react-icons/im";
 import {
-  AiFillStar,
   AiOutlineHome,
   AiOutlineFundProjectionScreen,
   AiOutlineUser,
 } from "react-icons/ai";
-
-//import { CgFileDocument } from "react-icons/cg";
 
 function NavBar() {
   const [expand, updateExpanded] = useState(false);
@@ -38,7 +31,12 @@ function NavBar() {
       className={navColour ? "sticky" : "navbar"}
     >
       <Container>
-        
+        {/* Add Navbar.Toggle for mobile view */}
+        <Navbar.Toggle 
+          aria-controls="responsive-navbar-nav" 
+          onClick={() => updateExpanded(expand => !expand)} 
+        /> 
+
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto" defaultActiveKey="#home">
             <p className="mynamenav">Fatima BaniSakher</p>
@@ -71,28 +69,12 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            {/* <Nav.Item>
-             <Nav.Link
-                as={Link}
-                to="/resume"
-                onClick={() => updateExpanded(false)}
-              >
-                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
-              </Nav.Link>
-            </Nav.Item>*/}
-
-            <Nav.Item>
-              
-            </Nav.Item>
-
             <Nav.Item className="fork-btn">
               <a
                 href="mailto:fatima.mbk00@gmail.com"
                 target="_blank"
                 className="fork-btn-inner"
               > 📩
-                {/*<CgGitFork style={{ fontSize: "1.2em" }} />{" "}
-                <AiFillStar style={{ fontSize: "1.1em" }} />*/}
               </a>
             </Nav.Item>
           </Nav>
